511cbb2a646833f4ba316253324d4b613fe83d70
ZhengHui <lim_z_hui@hotmail.com>
2023-02-14 16:27:54 +0800

    still unable to work

--- src/myapp/Main.java
+++ src/myapp/Main.java
@@ -1,15 +1,14 @@
 package myapp;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -27,41 +26,73 @@ public static void main(String[] args) {
             
             InputStream is = sock.getInputStream();
             ObjectInputStream ois = new ObjectInputStream(is);
-            InputStreamReader isr = new InputStreamReader(ois);
-            BufferedReader br = new BufferedReader(isr);
+            // InputStreamReader isr = new InputStreamReader(ois);
+            // BufferedReader br = new BufferedReader(isr);
 
             OutputStream os = sock.getOutputStream();
-            DataOutputStream dos = new DataOutputStream(os);
-            ObjectOutputStream oos = new ObjectOutputStream(dos);
+            // DataOutputStream dos = new DataOutputStream(os);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
 
 
-            String line;
+            String response = ois.readUTF();
+            System.out.println(response);
 
-            // while ((line = br.readLine()) != null) {
-            //     String[] cols = line.split(",");
-            //     System.out.println();
+            String[] serverInput = response.split(",");
             
-            // }
-             
-        //
+            float [] floatsInput = new float[serverInput.length];
 
-        // to return name in NRIC
-        dos.writeUTF("Received name: Lim Zheng Hui");
+                for (int i = 0; i < serverInput.length; i++) {
+                    floatsInput[i] = Float.parseFloat(serverInput[i]);
+                }
+                
+                float sum = 0f;
+                float mean = 0f;
 
-        // to return email add
-        dos.writeUTF("Received email: lim_z_hui@hotmail.com");
+                for (float f : floatsInput) {
+                    sum += f;                   
+                }
 
-        // to return mean calculated
-        dos.writeUTF("return calculated value");
+                mean = sum / floatsInput.length;
 
-        // to return standard deviation
-        dos.writeUTF("asdfas");
+            System.out.println("Sum of floats: " + sum);
+            System.out.println("Mean calculation: " + mean);
 
-      
+            // to calculate the standard deviation
+            float calculationPart = 0f;
+
+            for (float a : floatsInput) {
+                float eachDiff = a - mean;
+                calculationPart = calculationPart + (eachDiff * eachDiff);
+            }
+
+            float var = calculationPart / floatsInput.length;
+            float standDev = (float) Math.sqrt(var);
+
+            System.out.println("Standard deviation: " + standDev);
+
+           String name = "Lim Zheng Hui";
+           String email = "lim_z_hui@hotmail.com";
+
+            // to return name in NRIC
+            oos.writeUTF("Received name: " + name);
+
+            // to return email add
+            oos.writeUTF("Received email: " + email);
+
+            // to return mean calculated
+            oos.writeFloat(mean);
+
+            // to return standard deviation
+            oos.writeFloat(standDev);
+
+        
             
             is.close();
-            os.close();
+            ois.close();
             sock.close();
+
+            os.close();
+            oos.close();
 
         }catch(UnknownHostException e){
             e.printStackTrace();
@@ -70,7 +101,8 @@ public static void main(String[] args) {
         }catch(IOException e){
             e.printStackTrace();
         }
-    }
+    
+}
 
 }
 
